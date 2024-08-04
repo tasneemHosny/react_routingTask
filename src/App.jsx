c909146a88188d76a36d3aa3a6caac5f5fd852c7
tasneemHosny <tasneemhosny88@gmail.com>
2024-08-04 20:44:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import StartFramework from './Component/start_framework/start_framework';
+import About from './Component/about/about';
+import Portfolio from './Component/portfolio/portfolio';
+import Contact from './Component/contact/contact';
+import Layout from './Component/layout/layout';
 
-import './App.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import Start_framework from './Component/start_framework/start_framework'
-import About from './Component/about/about'
-import Portfolio from './Component/portfolio/portfolio'
-import Contact from './Component/contact/contact'
-import Layout from './Component/layout/layout'
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: 'start_framework', element: <StartFramework /> },
+        { path: '/', element: <StartFramework /> },
+        { path: 'about', element: <About /> },
+        { path: 'portfolio', element: <Portfolio /> },
+        { path: 'contact', element: <Contact /> },
+      ],
+    },
+  ],
+  { basename: '/RoutingTask_react' } // Set the basename here
+);
+
 function App() {
-  let router=createBrowserRouter(
-    [{path:"/",element:<Layout></Layout>,children:[
-      {path:"start_framework" , element:<Start_framework></Start_framework>},
-      {path:"/" , element:<Start_framework></Start_framework>},
-      {path:"about",element:<About></About>},
-      {path:"portfolio",element:<Portfolio></Portfolio>},
-      {path:"contact",element:<Contact></Contact>}
-    ]}]
-  )
   return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-    </>
-  )
+    <RouterProvider router={router}></RouterProvider>
+  );
 }
 
-export default App
+export default App;
